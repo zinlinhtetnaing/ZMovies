68f133a159f8fc4b4a3adb86498ae499f4435f62
zinlinhtetnaing <zinlinhtetnaing@acedatasystems.com>
2021-03-16 03:00:37 +0630

    Initial Commit

---
+++ ZMovies/ZMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZMoviesApp.swift
+//  ZMovies
+//
+//  Created by Zin Lin Htet Naing on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ZMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
